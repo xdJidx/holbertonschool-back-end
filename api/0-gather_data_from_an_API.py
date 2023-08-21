9c5bba9390d1dd2ebeee9f35de977301fe476ecd
xdJidx <bernardjerome59@gmail.com>
2023-08-21 14:38:37 +0200

    Check with other code

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -1,34 +1,35 @@
 #!/usr/bin/python3
 """
-script that, using this REST API, for a given employee ID,
-returns information about his/her TODO list progress.
+Python script that, using a REST API, for a given employee ID.
 """
+
 
 import requests
 import sys
 
 
 if __name__ == "__main__":
-
     employee_id = int(sys.argv[1])
-
     base_url = "https://jsonplaceholder.typicode.com"
-    user_url = f"{base_url}/users/{employee_id}"
-    todo_url = f"{base_url}/todos?userId={employee_id}"
+    user_id = f"{base_url}/users/{employee_id}"
+    todo = f"{base_url}/todos?userId={employee_id}"
 
-    user_response = requests.get(user_url)
-    todo_response = requests.get(todo_url)
+    response_user = requests.get(user_id)
+    response_todo = requests.get(todo)
 
-    user_data = user_response.json()
-    todo_data = todo_response.json()
+    user_data = response_user.json()
+    todo_data = response_todo.json()
 
-    employee_name = user_data['name']
-    total_number_of_task = len(todo_data)
-    number_of_done_tasks = sum(1 for task in todo_data if task['completed'])
-    completed_tasks_titles = [task['title']
-                              for task in todo_data if task['completed']]
+    name = user_data['name']
+
+    todo_done = sum(1 for task in todo_data if task['completed'])
+    todo_count = len(todo_data)
+
+    list_task_complete = [task['title']
+                          for task in todo_data if task['completed']]
 
     print("Employee {} is done with tasks({}/{}):"
-          .format(employee_name, number_of_done_tasks, total_number_of_task))
-    for task_title in completed_tasks_titles:
-        print("\t {}".format(task_title))
+          .format(name, todo_done, todo_count))
+
+    for title in list_task_complete:
+        print("\t {}".format(title))
